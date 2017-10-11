89310f0a3be722a24496950e62fcb5e3c94d70f4
Brian McRae <mcrae.brian@gene.com>
2017-10-11 13:12:35 -0700

    Add way to emit entered form data to browser console

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -3,7 +3,18 @@ var app = angular.module('instacart', []);
 app.controller('mainController', function($scope, $http) {
   $scope.showApplicationForm = false;
 
+  /**
+   * Toggles the display of the application form.
+   */
   $scope.toggleApplicationForm = function() {
     $scope.showApplicationForm = !$scope.showApplicationForm;
+  };
+
+  /**
+   * Submits the form data.
+   * Returns the form data to the browser's console as an object.
+   */
+  $scope.submitApplicationForm = function(formData) {
+    console.log(formData);
   };
 });
