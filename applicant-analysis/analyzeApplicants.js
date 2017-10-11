69abc3019d6b6caf5b55043e8cfc28a0ad4346f0
Brian McRae <mcrae.brian@gene.com>
2017-10-11 14:39:51 -0700

    Draft out approach for solving the problem

--- applicant-analysis/analyzeApplicants.js
+++ applicant-analysis/analyzeApplicants.js
@@ -17,3 +17,29 @@ rl.question('Please enter the start date : ', function(startDate) {
 
 var row = db.exec('SELECT * FROM applicants');
 // console.log(row);
+
+// Approach:
+// 1. Ingest the file into the database
+// 2. Create an empty object to store the counts by week and event (intended output object show below)
+// 3. For each row, figure out which week grouping it belongs in. Then, figure out which workflow_state bucket it belongs in.
+// If the week grouping or workflow_state bucket do not exist in the object output, then create them.
+// Initialize the value to be 0.
+// Else, if it does exist, then increment the count by 1.
+//
+// Intended object output:
+// var output = {
+//   '2014-07-14': {
+//     'hired': 102,
+//     'onboarding_completed': 191
+//   },
+//   '2014-08-04': {
+//     'hired': 343
+//   }
+// }
+//
+// Then, with the output object, can generate a csv of:
+// count,week,workflow_state
+// 102,2014-07-14,hired
+// 191,2014-07-14,onboarding_completed
+// 343,2014-08-04,hired
+//
