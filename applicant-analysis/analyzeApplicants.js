b99f75354dee4207a3f8587bd78dc2bd4e8f2fbc
Brian McRae <mcrae.brian@gene.com>
2017-10-11 14:12:25 -0700

    Attempt to read from sqlite3 file

--- applicant-analysis/analyzeApplicants.js
+++ applicant-analysis/analyzeApplicants.js
@@ -1,14 +1,19 @@
 var readline = require('readline');
+var Database = require('better-sqlite3');
+var options = {}
+var db = new Database('applicants.sqlite3', options);
 
 var rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-rl.question('Please enter the start date : ', (answer1) => {
-    rl.question('Please enter the end date : ', (answer2) => {
-        var result = answer1 + ' ' + answer2;
-        console.log(`${result}`);
-        rl.close();
-    });
+rl.question('Please enter the start date : ', function(startDate) {
+  rl.question('Please enter the end date : ', function(endDate) {
+    console.log(`start date: ${startDate} \nend date: ${endDate}`);
+    rl.close();
+  });
 });
+
+var row = db.exec('SELECT * FROM applicants');
+// console.log(row);
