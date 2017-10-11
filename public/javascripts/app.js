ea8d007607b28ab14ccd81f4ba098686da7c4d05
Brian McRae <mcrae.brian@gene.com>
2017-10-11 12:57:19 -0700

    Add a way for the application form to be displayed to user.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,4 +1,9 @@
 var app = angular.module('instacart', []);
 
 app.controller('mainController', function($scope, $http) {
+  $scope.showApplicationForm = false;
+
+  $scope.toggleApplicationForm = function() {
+    $scope.showApplicationForm = !$scope.showApplicationForm;
+  };
 });
